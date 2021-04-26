ccb68a788afea690129c7c7211e668e94c39affe
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-04-26 15:00:07 -0300

    Back to Basic
    Back to Basic.

---
+++ Aula3/ex.js
@@ -0,0 +1,3 @@
+let nome = "Ed"
+let hora = 10
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript as ${hora} da manhã.`)
---
+++ Aula4/aula8.js
@@ -0,0 +1,8 @@
+ const nome = 'Ed';
+ const sobrenome = 'Paz';
+ const idade = 37;
+ const peso = 97;
+ const alturaEmM = 1.80;
+ let imc = peso / (alturaEmM * alturaEmM);
+ anoNascimento = 2021 - idade;
+ console.log(anoNascimento)
