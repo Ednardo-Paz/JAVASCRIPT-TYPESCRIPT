7f40f907290e869d0ac377803282cd027e738a36
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-02 09:59:50 -0300

    Up
    Up

--- exercício3/assets/js/main.js
+++ exercício3/assets/js/main.js
@@ -1,10 +1,11 @@
-function FizzBuss(n) {
-  if (typeof n !== `number`) return n;
-  if (n % 3 === 0 && n % 5 === 0) return `FizzBuzz`;
-  if (n % 3 === 0) return `fizz`;
-  if (n % 5 === 0) return `Buss`;
+function FizzBuzz(n){
+   if ( typeof n !== `number`) return n;
+  if(n % 3 === 0 && n % 5 === 0) return `FizzBuzz`;
+  if(n % 3 === 0) return `fizz`;
+  if(n % 5 === 0) return `buzz`;
   return n;
-}console.log(`a`, FizzBuss(`a`))
-for(let i = 0; i < 100; i++){
-  console.log(i, FizzBuss(i))
+}
+
+for(let i = 0; i <= 100; i++){
+  console.log(i, FizzBuzz(i))
 }
