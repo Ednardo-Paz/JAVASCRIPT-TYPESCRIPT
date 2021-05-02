e55c0d56613ae9b0b68db05f62c2cd26b166a6b1
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-01 23:57:19 -0300

    UP
    UP

--- aula20/js.js
+++ aula20/js.js
@@ -1,25 +1,30 @@
-function meuEscope(){
+function meuEscopo() {
     let form = document.querySelector(`.form`);
     let res = document.querySelector(`.res`);
-
     let pessoas = [];
 
-    function receberEvento(evento){
-        evento.preventDefault();
+    function receberEvento(e) {
+        e.preventDefault();
         let nome = document.querySelector(`.nome`);
         let sobrenome = document.querySelector(`.sobrenome`);
-        let peso = document.querySelector(`.peso`);
+        let peso= document.querySelector(`.peso`);
         let altura = document.querySelector(`.altura`);
 
         pessoas.push({
             nome: nome.value,
             sobrenome: sobrenome.value,
             peso: peso.value,
-            altura: altura.value,
-        });
-        console.log(pessoas);
+            altura: altura.value
+        })
+    
+        res.innerHTML = `${nome.value}, ${sobrenome.value}`
+
+       
     }
-        
+
     form.addEventListener(`submit`, receberEvento);
+  
+    
+
 }
-meuEscope();
+meuEscopo();
