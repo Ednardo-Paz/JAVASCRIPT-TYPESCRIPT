20e6ec91fb307935e463365144df45adcfed910a
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-03 20:58:44 -0300

    up
    up

--- modelo.1/modelo.1/assets/js/main.js
+++ modelo.1/modelo.1/assets/js/main.js
@@ -8,80 +8,84 @@ function criarLi() {
   let li = document.createElement(`li`);
   return li;
 }
+
+
 //4- Evento para add tarefa com o enter.
 inputTarefa.addEventListener(`keypress`, function (e) {
   if (e.keyCode === 13) {
-    if (!inputTarefa) return
-    criarTarefa(inputTarefa.value);
+    criarTarefa(inputTarefa.value)
   }
 })
 
-//-5 função limpa input
-function limpaIMput() {
-  inputTarefa.value = ``;
-  inputTarefa.focus();
 
+//-5 função limpa input
+function limpaTarefa() {
+  inputTarefa.value = ` `;
+  inputTarefa.focus();
 }
+
 //-6 criar botão apagar em uma LI e add 
 function btnApagar(li) {
-  li.innerText += ` `;
+  li.innerHTML += ` `;
   let btnApagar = document.createElement(`button`);
-  btnApagar.innerText = `Apagar`
-  li.appendChild(btnApagar)
-  btnApagar.setAttribute(`class`, `apagar`)
+  btnApagar.innerHTML = `Apagar`;
+  li.appendChild(btnApagar);
+  btnApagar.setAttribute(`class`, `apagar`);
+  btnApagar.setAttribute(`title`, `Tarefa concluida.`)
+
 }
-//2 - Pegar o valor do input e criar Li.
+
+//2 - Pegar o valor do input e criar Li. 
 function criarTarefa(textoInput) {
   let li = criarLi();
   li.innerHTML = textoInput
-  tarefas.appendChild(li);
-  limpaIMput();
+  tarefas.appendChild(li)
+  limpaTarefa()
   btnApagar(li)
-  salvarTarefa();
+  SalvarTarefa()
 }
+
+
 // //7 criar evento para o btn-apagar
 document.addEventListener(`click`, function (e) {
   let el = e.target
   if (el.classList.contains(`apagar`)) {
-    el.parentElement.remove();
-    salvarTarefa();
+    el.parentElement.remove()
+    SalvarTarefa()
   }
+
 })
+
 // //8- Salvar tarefas a li com array das tarefas e modificando o texto para a pagart o texto 
-function salvarTarefa() {
-  let liTarefas = tarefas.querySelectorAll(`li`);
+function SalvarTarefa() {
+  let liTarefa = tarefas.querySelectorAll(`li`)
   let listaDeTarefas = [];
 
-  for (let tarefa of liTarefas) {
-    let tarefaTexto = tarefa.innerText;
-    tarefaTexto = tarefaTexto.replace(`Apagar`, ` `).trim();
+  for (let tarefa of liTarefa) {
+    let tarefaTexto = tarefa.innerText
+    tarefaTexto = tarefaTexto.replace(`Apagar`, ` `);
     listaDeTarefas.push(tarefaTexto)
-  }
 
-  let tarefaJson = JSON.stringify(listaDeTarefas);
-  localStorage.setItem(`tarefas`, tarefaJson); 
+    let tarefaJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem(`tarefas`, tarefaJSON);
+   
+  }
 }
 
 //9- add tarefas salvas trazendo de volta para arrays.
-
- function addTarefasSalvas(){
+function adicionaTarefasSalvas(){
   let tarefas = localStorage.getItem(`tarefas`);
   let listaDeTarefas = JSON.parse(tarefas)
-  console.log(listaDeTarefas)
-
+  
   for(let tarefa of listaDeTarefas){
-     criarTarefa(tarefa)
-    
-   }
+    criarTarefa(tarefa)
+  }
 }
-addTarefasSalvas();
 
+adicionaTarefasSalvas()
 
- //1- Criar evento click e não validar se náo tiver valor. Function criar tarefa.
+//1- Criar evento click e não validar se náo tiver valor. Function criar tarefa.
 btnTarefa.addEventListener(`click`, function (e) {
-  if (!inputTarefa) return
-  criarTarefa(inputTarefa.value);
+  if (!inputTarefa.value) return;
+  criarTarefa(inputTarefa.value)
 })
-
-
-
