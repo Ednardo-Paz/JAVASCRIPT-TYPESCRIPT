9fb23ef1bbe98356464d53df5496ac3df0fcf615
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-15 14:27:28 -0300

    up
    	up

--- teste/assets/js/main.js
+++ teste/assets/js/main.js
@@ -1,4 +1,15 @@
-const quad = document.querySelectorAll(`body *`);
+const quad = document.querySelectorAll(`h1`);
 
+function testar(e) {
 
+   quad.forEach((e) => {
+       e.classList.add(`quadrado-blue`)
+   })
+
+   e.target.classList.remove(`quadrado-blue`)
+}
+
+quad.forEach((item) => {
+  item.addEventListener(`click`, testar);
+});
 
