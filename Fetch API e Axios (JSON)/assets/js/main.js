49ef5b12f8697a01bc91ad548f0f5cd6715fd674
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-26 20:35:10 -0300

    113. Fetch API e Axios (JSON)
    113. Fetch API e Axios (JSON)

--- Fetch API e Axios (JSON)/assets/js/main.js
+++ Fetch API e Axios (JSON)/assets/js/main.js
@@ -1,27 +1,62 @@
-fetch('pessoas.json')
-  .then(resposta => resposta.json())
-  .then(json => carregaElementosNaPagina(json));
+// fetch(`pessoas.json`)
+//   .then(resposta => resposta.json())
+//   .then(json =>  carregaElementosNaPagina(json));
+axios(`pessoas.json`)
+  .then(resposta => carregaElementosNaPagina(resposta.data))
 
-// axios('pessoas.json')
-//   .then(resposta => carregaElementosNaPagina(resposta.data));
+  function carregaElementosNaPagina(json) {
+    const table = document.createElement(`table`);
+    for(let pessoa of json) {
+      const tr = document.createElement(`tr`);
 
-function carregaElementosNaPagina(json) {
-  const table = document.createElement('table');
+      let td = document.createElement(`td`);
+      td.innerHTML = pessoa.nome;
+      tr.appendChild(td);
 
-  for(let pessoa of json) {
-    const tr = document.createElement('tr');
+      td = document.createElement(`td`);
+      td.innerHTML = pessoa.idade;
+      tr.appendChild(td);
 
-    let td1 = document.createElement('td');
-    td1.innerHTML = pessoa.nome;
-    tr.appendChild(td1);
+      td = document.createElement(`td`);
+      td.innerHTML = pessoa.salario;
+      tr.appendChild(td);
 
-    let td2 = document.createElement('td');
-    td2.innerHTML = pessoa.idade;
-    tr.appendChild(td2);
+      table.appendChild(tr);
+     
+    }
 
-    table.appendChild(tr);
+    const resultado = document.querySelector(`.resultado`);
+    resultado.appendChild(table);
   }
 
-  const resultado = document.querySelector('.resultado');
-  resultado.appendChild(table);
-}
+
+
+
+
+// fetch('pessoas.json')
+//   .then(resposta => resposta.json())
+//   .then(json => carregaElementosNaPagina(json));
+
+// // axios('pessoas.json')
+// //   .then(resposta => carregaElementosNaPagina(resposta.data));
+
+// function carregaElementosNaPagina(json) {
+//   const table = document.createElement('table');
+
+//   for(let pessoa of json) {
+//     const tr = document.createElement('tr');
+
+//     let td1 = document.createElement('td');
+//     td1.innerHTML = pessoa.nome;
+//     tr.appendChild(td1);
+
+//     let td2 = document.createElement('td');
+//     td2.innerHTML = pessoa.idade;
+//     tr.appendChild(td2);
+
+//     table.appendChild(tr);
+//   }
+
+//   const resultado = document.querySelector('.resultado');
+//   resultado.appendChild(table);
+// }
